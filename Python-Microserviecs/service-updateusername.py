b248fde687a3ff993e3ae1c9491d2915d5a76c33
Neta4302 <54069403+Neta4302@users.noreply.github.com>
2023-01-24 11:10:08 +0700

    Update service-updateusername.py

--- Python-Microserviecs/service-updateusername.py
+++ Python-Microserviecs/service-updateusername.py
@@ -6,22 +6,22 @@ import data_user as us
 
 app = Flask(__name__)
 
-@app.route('/update/<user>', methods=['POST'])
+@app.route('/update/<user>', methods=['PUT'])
 def update(user):
     # Get the user's login information from the request
     user = request.form.get('username')
     passwd = request.form.get('password')
     name = request.form.get('name')
 
-    _user = us.find_username(user)
+    _user = us.user_name()
     data = [x for x in _user if x["user"]==user]
     # return jsonify(_user)
     #Get Data
     if (data):
+        return jsonify({'error': 'Fail to update!'}), 404
+    else:
         us.update_user(user,passwd,name)
         return jsonify({'message': 'Updated successfully.'}), 200
-    else:
-        return jsonify({'error': 'Fail to update!'}), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5004, debug=True) #127.0.0.1
